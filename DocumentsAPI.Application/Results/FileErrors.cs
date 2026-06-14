878815b27031ea92d38be90dc98b46baa2537236
NeedX <hramko2016@yandex.ru>
2026-06-14 23:15:11 +0300

    feat: added consumer for pdf

---
+++ DocumentsAPI.Application/Results/FileErrors.cs
@@ -0,0 +1,26 @@
+﻿using Services.Application.Results;
+
+namespace DocumentsAPI.Application.Errors;
+
+public static class FileErrors
+{
+    public static readonly Error Empty = new(
+        "File.Empty",
+        "File was empty.",
+        ErrorType.Validation);
+
+    public static readonly Error NotFound = new(
+        "File.NotFound",
+        "File not found.",
+        ErrorType.NotFound);
+
+    public static readonly Error DownloadFailed = new(
+        "File.DownloadFailed",
+        "Error occurred while downloading the file.",
+        ErrorType.Failure);
+
+    public static readonly Error DeleteFailed = new(
+        "File.DeleteFailed",
+        "Error occurred while deleting the file.",
+        ErrorType.Failure);
+}
--- DocumentsAPI.Infrastructure/Extensions/DependencyInjection.cs
+++ DocumentsAPI.Infrastructure/Extensions/DependencyInjection.cs
@@ -1,8 +1,10 @@
 ﻿using Azure.Storage.Blobs;
 using DocumentsAPI.Application.Abstractions;
 using DocumentsAPI.Infrastructure.Storage;
+using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace DocumentsAPI.Infrastructure.Extensions;
 
@@ -12,6 +14,25 @@ public static class DependencyInjection
     {
         services.AddSingleton<IBlobService, BlobService>();
         services.AddSingleton(_ => new BlobServiceClient(configuration.GetConnectionString("BlobStorage")));
+
+        services.AddMassTransit(busConfigurator =>
+        {
+            busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+            busConfigurator.AddConsumers(Assembly.GetExecutingAssembly());
+
+            busConfigurator.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host(configuration["RabbitMQ:Host"], "/", h =>
+                {
+                    h.Username(configuration["RabbitMQ:Username"]);
+                    h.Password(configuration["RabbitMQ:Password"]);
+                });
+
+                cfg.ConfigureEndpoints(context);
+            });
+        });
+
         return services;
     }
 }
